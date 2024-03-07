c56f4b49e4dbcea038b169434fe86dc38846ef4f
Xgamer6 <142106680+Xgamer6@users.noreply.github.com>
2024-03-07 06:01:41 +0100

    Create SendWebhookTask.php

---
+++ src/SendWebhookTask.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace xgamer6\MTD\task;
+
+use pocketmine\scheduler\AsyncTask;
+use pocketmine\Server;
+use pocketmine\utils\Internet;
+
+class SendWebhookTask extends AsyncTask {
+
+    private $webhookUrl;
+    private $message;
+
+    public function __construct(string $webhookUrl, string $message) {
+        $this->webhookUrl = $webhookUrl;
+        $this->message = $message;
+    }
+
+    public function onRun() {
+        $data = [
+            "content" => $this->message
+        ];
+
+        Internet::postURL($this->webhookUrl, json_encode($data), 5, [], "application/json");
+    }
+}
