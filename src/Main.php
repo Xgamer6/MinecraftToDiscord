deb0f86acaabe849710aa640d7f87f8730847a3d
Xgamer6 <142106680+Xgamer6@users.noreply.github.com>
2024-03-07 05:49:39 +0100

    Create Main.php

---
+++ src/Main.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace xgamer6\MTD;
+
+use pocketmine\plugin\PluginBase;
+
+class YourPluginName extends PluginBase {
+
+    public function onEnable() {
+        $this->getLogger()->info("§aPlugin got started!");
+    }
+}
